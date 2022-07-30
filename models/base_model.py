09407d684c81006307205dfde17ba9ca2e7fdc32
marlon <marlon@debian-BULLSEYE-live-builder-AMD64>
2022-07-30 14:45:47 +0200

    AirBnB Clone

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,7 @@ class BaseModel:
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
         storage.save()
 
